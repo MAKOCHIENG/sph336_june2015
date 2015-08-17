f98c3983005c586288a91c7144481a28c1ef489d
MAKOCHIENG <fredphilip01@gmail.com>
2015-08-17 13:24:08 +0300

    updated files

--- twrk60/adc.h
+++ twrk60/adc.h
@@ -7,13 +7,24 @@
 #define ADC1_H_
 
 #include "MK60DZ10.h"
-#include "adc16.h"
 
-//Configuration
+#define A                 0x0
+#define B                 0x1
+
+#define COCO_NOT          0x00
+
+#define CALF_FAIL          ADC_SC3_CALF_MASK
+
+#define AVGS_4             0x00
+#define AVGS_8             0x01
+#define AVGS_16            0x02
+#define AVGS_32            0x03
+
+//Configuration for single-ended mode
 void ADC1_Init16b(void);
 uint16_t ADC1_Read16b(uint8_t channelNumber);
 
-//Calibration
-uint8_t ADC_Cal(ADC_MemMapPtr adcmap);
+//Calibration for single-ended mode
+uint8_t ADC_CalSingle(ADC_MemMapPtr adcmap);
 
 #endif /* ADC1_H_ */
