c15b175c9edbedd55dfc8dc4d93ba73cae67a87d
MAKOCHIENG <fredphilip01@gmail.com>
2015-08-17 13:47:35 +0300

    I meant modified main.c not main.h

--- twrk60/main.c
+++ twrk60/main.c
@@ -51,7 +51,7 @@ int main(void)
 
 
 	//initialize system
-	SystemInit();
+	 SystemInit();
 
 	//initialize UART5 with 8-N-1 settings, 57600 baudrate
 	init_uart(UART5_BASE_PTR,periph_clk_khz,57600);
